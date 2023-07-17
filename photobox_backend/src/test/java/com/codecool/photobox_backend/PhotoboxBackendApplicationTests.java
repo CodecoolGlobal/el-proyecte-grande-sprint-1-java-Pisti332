71e9007b19e37afb69582f799da9817ecab27a45
Pisti332 <varga.istvan.p1@gmail.com>
2023-07-17 14:58:31 +0200

    init backend

---
+++ photobox_backend/src/test/java/com/codecool/photobox_backend/PhotoboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.photobox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoboxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
