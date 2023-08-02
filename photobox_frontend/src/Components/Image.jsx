a746d30fde084861d099dd8a565049d8d647ad55
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-08-02 16:14:51 +0200

    rename Picture to Image

--- photobox_frontend/src/Components/Image.jsx
+++ photobox_frontend/src/Components/Image.jsx
@@ -6,18 +6,18 @@ import {
 } from '@mui/material';
 
 
-const Picture = ({ picture }) => {
+const Image = ({ image }) => {
     return (
         <>
             <Card sx={{ width: '80vw', marginRight: '20px', marginTop: '10px' }}>
                 <CardMedia
                     sx={{ height: '60vh' }}
-                    image={`/img/${picture}`}
-                    title={picture}
+                    image={`/img/${image}`}
+                    title={image}
                 />
                 <CardContent>
                     <Typography gutterBottom variant='h5' component='div'>
-                        {picture}
+                        {image}
                     </Typography>
                     <Typography variant='body2' color='text.secondary'>
                         Todo...
@@ -28,4 +28,4 @@ const Picture = ({ picture }) => {
     );
 };
 
-export default Picture;
+export default Image;
