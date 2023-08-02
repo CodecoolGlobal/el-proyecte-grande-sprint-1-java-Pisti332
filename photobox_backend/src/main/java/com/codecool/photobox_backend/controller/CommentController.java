19ebc1d8da298c2a7dd3f3525a07d6072b1c5232
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-08-02 10:42:34 +0200

    handle adding new comment

--- photobox_backend/src/main/java/com/codecool/photobox_backend/controller/CommentController.java
+++ photobox_backend/src/main/java/com/codecool/photobox_backend/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.codecool.photobox_backend.controller;
 
+import com.codecool.photobox_backend.controller.dtos.comment.CommentDTO;
 import com.codecool.photobox_backend.model.Comment;
 import com.codecool.photobox_backend.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import java.util.Set;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("api")
 public class CommentController {
   private CommentService commentService;
 
@@ -18,13 +20,13 @@ public class CommentController {
     this.commentService = commentService;
   }
 
-  @GetMapping("/{user_id}/{image_id}")
+  @GetMapping("{user_id}/{image_id}")
   public Set<Comment> getComments(@PathVariable Long user_id, @PathVariable UUID image_id) {
     return commentService.getComments(user_id, image_id);
   }
 
-  @PostMapping("/{user_id}/{image_id}")
-  public void addComment(@RequestBody Comment comment, @PathVariable Long user_id, @PathVariable UUID image_id){
-    commentService.addComment(comment,user_id,image_id);
+  @PostMapping("comment")
+  public void addComment(@RequestBody CommentDTO comment){
+    commentService.addComment(comment);
   }
 }
--- photobox_backend/src/main/java/com/codecool/photobox_backend/service/CommentService.java
+++ photobox_backend/src/main/java/com/codecool/photobox_backend/service/CommentService.java
@@ -1,6 +1,9 @@
 package com.codecool.photobox_backend.service;
 
+import com.codecool.photobox_backend.controller.dtos.comment.CommentDTO;
 import com.codecool.photobox_backend.model.Comment;
+import com.codecool.photobox_backend.model.Image;
+import com.codecool.photobox_backend.model.User;
 import com.codecool.photobox_backend.repository.CommentRepository;
 import com.codecool.photobox_backend.repository.ImageRepository;
 import com.codecool.photobox_backend.repository.UserRepository;
@@ -26,7 +29,10 @@ public class CommentService {
     return imageRepository.getById(imageId).getComments();
   }
 
-  public void addComment(Comment comment, Long userId, UUID imageId) {
-    imageRepository.getById(imageId).getComments().add(comment);
+  public void addComment(CommentDTO comment) {
+    User user = userRepository.getById(comment.userId());
+    Image image = imageRepository.getById(comment.imageId());
+    Comment newComment = new Comment(comment.content(),user,image);
+    commentRepository.save(newComment);
   }
 }
