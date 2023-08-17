f01c8a6f4dfcf78aa496144e1422152777dfbca5
Pisti332 <varga.istvan.p1@gmail.com>
2023-08-17 11:41:19 +0200

    hide user id from client

--- photobox_backend/src/main/java/com/codecool/photobox_backend/model/User.java
+++ photobox_backend/src/main/java/com/codecool/photobox_backend/model/User.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @JsonIgnore
     private Long id;
     @Column(unique = true)
     private String name;
